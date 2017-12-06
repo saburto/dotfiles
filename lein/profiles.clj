7c806a24d3e39e59d9edc68ea4ae71268e1a94eb
Sebastian Aburto <seba.aburto@gmail.com>
2017-12-06 11:27:44 -0300

    Add lein profile

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.15.1"]
+                  [lein-autoexpect "1.9.0"]
+                  [com.jakemccrary/lein-test-refresh "0.21.1"]
+                  [luminus/lein-template "2.9.10.74"]]}}
